c93722eb090c40d14179d8546f7f1f09e0db77f8
loyolaAcademyCS <swhalen@loy.org>
2023-02-22 14:55:45 -0600

    "Script commit"

---
+++ pset62.java
@@ -0,0 +1,58 @@
+/* This is pset62. Follow each instruction to the letter. If you are not 
+ * explicitly instructed to do something, then you don't need to do it, 
+ * even if you were used to doing it in the past. 
+ */
+
+public class pset62{
+    public static void main(String[] args){
+		/* 1) Declare and initialize a String array to contain the 
+		 * values "I", "love", and "git", in that order. You 
+		 * MUST call this array strArr.
+		*/
+		
+		/* 2) Declare and initialize an int array of length 50. Then set its 
+		 * first and second values to 1. This array MUST be called fibArr. 
+		 * Don't change any of the other 48 values.
+		*/
+		
+		/* 3) Declare and initialize a double array to contain the values 
+		 * 50.7, 302.23, 1.0, and 1245.134, in that order. You MUST call your 
+		 * array fibArr.
+		*/
+		
+		/* 7) Call your printStringArray method on strArr.*/
+		
+		/* 8) Call your sumDoubleArray method on dbArr.*/
+		
+		/* 9) Call your fibFifty method on intArr */
+    
+	}
+
+	/* 4) Write a public static method called printStringArray that 
+	 * prints out the contents of strArr, each on their own line.
+	*/
+	public static void printStringArray(String[] arr){
+		System.out.println("This method has yet to be implemented...");
+	}
+	/* 5) Write a public static method called sumDoubleArray that sums up 
+	 * all of the values in a double array and returns the sum.
+	*/
+	public static double sumDoubleArray(double[] arr){
+		System.out.println("This method has yet to be implemented...");
+		return -1.0; //Make sure to edit this return statement when you
+					 //write the method.
+	}
+
+	/* 6) Write a public static method called fibFifty that takes an int array 
+	 * as its sole parameter and edits the 3rd through 50th spots in it to 
+	 * contain the 3rd-50th fibonacci numbers. You can assume that the array 
+	 * it takes will always have values of 1 in their first two spots 
+	 * (which are the first two fibonacci numbers) and 0s in the 3rd through 
+	 * 50th spots. fibFifty must return an int array with the first 50 
+	 * fibonacci numbers.*/
+	public static int[] fibFifty(int[] arr){
+		System.out.println("This method has yet to be implemented...");
+		return null;	//Make sure to change this return statement when
+						//you write the method.
+	}
+}
