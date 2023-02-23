5ef0ca919f6042340882e394724f1ac1190c3eda
Seaver Olson <46256937+seaver-olson@users.noreply.github.com>
2023-02-23 17:10:57 +0000

    I couldn't check my homework because my env uses a different version of java In the future could you keep gmh as a .java file so I can compile it and check my work? Thanks

--- pset62.java
+++ pset62.java
@@ -3,44 +3,49 @@
  * even if you were used to doing it in the past. 
  */
 
-public class pset62{
+ public class pset62{
     public static void main(String[] args){
 		/* 1) Declare and initialize a String array to contain the 
 		 * values "I", "love", and "git", in that order. You 
 		 * MUST call this array strArr.
 		*/
-		
+		String[] strArr = {"I","love","git"};
 		/* 2) Declare and initialize an int array of length 50. Then set its 
 		 * first and second values to 1. This array MUST be called fibArr. 
 		 * Don't change any of the other 48 values.
 		*/
-		
+		int[] fibArr = new int[50];
+		fibArr[0] = 1;
+		fibArr[1] = 1;
 		/* 3) Declare and initialize a double array to contain the values 
 		 * 50.7, 302.23, 1.0, and 1245.134, in that order. You MUST call your 
 		 * array fibArr.
 		*/
-		
+		double[] dbArr = {50.7,302.23,1.0,1245.134};
 		/* 7) Call your printStringArray method on strArr.*/
-		
+		printStringArray(strArr);
 		/* 8) Call your sumDoubleArray method on dbArr.*/
-		
+		sumDoubleArray(dbArr);
 		/* 9) Call your fibFifty method on intArr */
-    
+		fibFifty(fibArr);
 	}
 
 	/* 4) Write a public static method called printStringArray that 
 	 * prints out the contents of strArr, each on their own line.
 	*/
 	public static void printStringArray(String[] arr){
-		System.out.println("This method has yet to be implemented...");
-	}
+		for(String e:arr){
+			System.out.println(e);
+	}}
 	/* 5) Write a public static method called sumDoubleArray that sums up 
 	 * all of the values in a double array and returns the sum.
 	*/
 	public static double sumDoubleArray(double[] arr){
-		System.out.println("This method has yet to be implemented...");
-		return -1.0; //Make sure to edit this return statement when you
-					 //write the method.
+		double sum = 0;
+		for(double e:arr){
+			sum += e;
+		}
+		return sum;
 	}
 
 	/* 6) Write a public static method called fibFifty that takes an int array 
@@ -51,8 +56,9 @@ public class pset62{
 	 * 50th spots. fibFifty must return an int array with the first 50 
 	 * fibonacci numbers.*/
 	public static int[] fibFifty(int[] arr){
-		System.out.println("This method has yet to be implemented...");
-		return null;	//Make sure to change this return statement when
-						//you write the method.
+		for(int i = 2; i < arr.length; i++){
+			arr[i] = arr[i-1] + arr[i-2];
+		}
+		return arr;
 	}
 }
